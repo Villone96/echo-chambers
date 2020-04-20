f4dbb71a98b5f8299534cb07af3f574a8894c65b
Giacomo Villa <giacomo.villa.mi@gmail.com>
2020-04-20 15:53:35 +0200

    compacted add edge method, added cleaning operation for username (COVID19)

--- src/preprocessing/ops_build_graph.py
+++ src/preprocessing/ops_build_graph.py
@@ -1,6 +1,7 @@
 from preprocessing.ops_on_raw_data import check_directory_absence
 import networkx as nx 
 import pandas as pd
+from tqdm import tqdm
 import os
 import ast
 
@@ -62,20 +63,15 @@ def build_covid_graph(path):
     df.dropna(axis='index', how='all', subset=['text_con_hashtag'], inplace = True)
     # G = nx.DiGraph()
     G = nx.MultiDiGraph()
-    n_row = 0
-    for _, row in df.iterrows():
-        n_row += 1
-        if n_row % 100000 == 0:
-            print("WE ARE IN LINE: ", n_row)
+    for _, row in tqdm(df.iterrows(), desc = "Rows processed"):
         if row[4] == 'self':
-            # G = add_edge(G, row[5], 'covid', row[0], row[2], 0, row[3], row[3])
-            G = add_edge_multiDiGraph(G, row[5], 'covid', row[0], row[2], 0, row[3], row[3])
+            G = add_edge(G, row[5], 'covid', row[0], row[2], 0, row[3], row[3], True)
         else:
             mentions = row[4].split(',')
             for mention in mentions:
-                mention.strip()
-                # G = add_edge(G, row[5], 'covid', row[0], row[2], 0, row[3], mention)
-                G = add_edge_multiDiGraph(G, row[5], 'covid', row[0], row[2], 0, row[3], mention)
+                mention = mention.strip()
+                if mention != '' and mention != '@':
+                    G = add_edge(G, row[5], 'covid', row[0], row[2], 0, row[3], mention, True)
 
 
     print('')
@@ -110,13 +106,13 @@ def nodes_management(G, option, multi, threshold = 0):
         for node in G.nodes():
             for edge in G.in_edges(node, data = True):
                 if not multi:
-                    total_edge += len(edge[2]['tweets'])
+                    total_edge += edge[2]['nums']
                 else:
                     total_edge += 1
             
             for edge in G.out_edges(node, data = True):
                 if not multi:
-                    total_edge += len(edge[2]['tweets'])
+                    total_edge += edge[2]['nums']
                 else:
                     total_edge += 1
 
@@ -136,65 +132,84 @@ def nodes_management(G, option, multi, threshold = 0):
         print('BAD OPTION VALUE - TRY REMOVE OR COUNT')
         return -1
 
+# Yes, I know, would be better RE :(
+def clean(username):
+    username = str(username)
+    username = username.strip()
+    username = username.split('\u2069')[0]
+    username = username.split('.')[0]
+    username = username.split("'")[0]
+    username = username.split(")")[0]
+    username = username.split("!")[0]
+    username = username.split("?")[0]
+    username = username.split('https')[0]
+    username = username.split('http')[0]
+    username = username.split('&#8217;s')[0]
+    username = username.split('’')[0]
+    username = username.split('より')[0]
+    username = username.split('さんから')[0] 
+    username = username.split('から')[0] 
+    if username[-3:] == 'pic' and username.lower() != 'apic':
+        username = username.split('pic')[0]
+    username = username.lower()
+    return username
 
-def add_edge_multiDiGraph(G, tweet, hashtag, likes, retweets, replies, source, destination):
-    if hashtag == 'covid':
-        G.add_edge(source, destination, tweet = tweet, likes = likes, retweets = retweets)
-    else:
-        G.add_edge(source, destination, tweet = tweet, hashtag = hashtag, likes = likes, 
-            retweets = retweets, replies = replies)
-    return G
-
-
-def add_edge(G, tweet, hashtag, likes, retweets, replies, source, destination):
-    if G.has_edge(source, destination):
-        G[source][destination]['tweets'].append(tweet)
-        G[source][destination]['likes'].append(likes)
-        G[source][destination]['retweets'].append(retweets)
-        G[source][destination]['nums']+= 1
-        if hashtag != 'covid':
-            G[source][destination]['hashtags'].append(hashtag)
-            G[source][destination]['replies'].append(replies)
-    else:
+def add_edge(G, tweet, hashtag, likes, retweets, replies, source, destination, multi):
+    source = clean(source)
+    destination = clean(destination)
+    if multi:
         if hashtag == 'covid':
-            G.add_edge(source, destination, tweets=[tweet], likes = [likes], retweets = [retweets], nums = 1)
+            G.add_edge(source, destination, tweet = tweet, likes = likes, retweets = retweets)
         else:
-            G.add_edge(source, destination, tweets=[tweet], hashtags=[hashtag], likes = [likes], 
-            retweets = [retweets], replies = [replies], nums = 1)
-    return G
+            G.add_edge(source, destination, tweet = tweet, hashtag = hashtag, likes = likes, 
+                retweets = retweets, replies = replies)
+        return G
+    else:
+        if G.has_edge(source, destination):
+            G[source][destination]['tweets'].append(tweet)
+            G[source][destination]['likes'].append(likes)
+            G[source][destination]['retweets'].append(retweets)
+            G[source][destination]['nums']+= 1
+            if hashtag != 'covid':
+                G[source][destination]['hashtags'].append(hashtag)
+                G[source][destination]['replies'].append(replies)
+        else:
+            if hashtag == 'covid':
+                G.add_edge(source, destination, tweets=[tweet], likes = [likes], retweets = [retweets], nums = 1)
+            else:
+                G.add_edge(source, destination, tweets=[tweet], hashtags=[hashtag], likes = [likes], 
+                retweets = [retweets], replies = [replies], nums = 1)
+        return G
 
 
 def build_vaccination_graph(path):
     df = pd.read_csv(path + '/final_data/' + 'Final_data.csv', lineterminator='\n')
-    G = nx.DiGraph()
-    # G = nx.MultiDiGraph()
+    # G = nx.DiGraph()
+    G = nx.MultiDiGraph()
     num_nodes = 0
     num_edges = 0
-    for _, row in df.iterrows():
+    for _, row in tqdm(df.iterrows(), desc="Rows processed"):
         num_nodes += 1
         mentions = ast.literal_eval(row[3])
         if 'self' in mentions:
-            G = add_edge(G, row[2], row[7], row[6], row[5], row[4], row[1], row[1])
-            # G = add_edge_multiDiGraph(G, row[2], row[7], row[6], row[5], row[4], row[1], row[1])
+            G = add_edge(G, row[2], row[7], row[6], row[5], row[4], row[1], row[1], True)
             num_edges += 1
         else:
             for mention in mentions:
-                G = add_edge(G, row[2], row[7], row[6], row[5], row[4], row[1], mention)
-                # G = add_edge_multiDiGraph(G, row[2], row[7], row[6], row[5], row[4], row[1], mention)
+                G = add_edge(G, row[2], row[7], row[6], row[5], row[4], row[1], mention, True)
                 num_edges += 1
                 
 
-
     G.name = 'Starter Vaccination Graph'
     print(nx.info(G))
-    print("{:<20}{:<8}".format('Real number of Edges: ', nodes_management(G, 'count', False)))
+    print("{:<20}{:<8}".format('Real number of Edges: ', nodes_management(G, 'count', True)))
 
-    final_G = nodes_management(G, 'remove', False, 3)
+    final_G = nodes_management(G, 'remove', True, 3)
     final_G.name = 'Final Vaccination Graph'
 
     print()
     print(nx.info(final_G))
-    print("{:<20}{:<8}".format('Real number of Edges: ', nodes_management(final_G, 'count', False)))
+    print("{:<20}{:<8}".format('Real number of Edges: ', nodes_management(final_G, 'count', True)))
     print('---------------------------------------')
     nx.write_gml(final_G, path + '/Graph/vaccination.gml')
 
